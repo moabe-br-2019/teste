2a34bbe30d2d50ccb8cb7127e09231f973ce5ff5
moabe-br-2019 <moabe@protonmail.com>
2022-08-08 17:17:28 -0300

    js

--- index.js
+++ index.js
@@ -4,6 +4,29 @@ window.onload = function(){
 
     //ver senha
 
-    const btnVerPass = document.querySelector("#form-pass")
-    const btnVerPassConfirm = document.querySelector("#form-pass-confirm")
+    //inputs 
+    const inputVerPass = document.querySelector("#form-pass")
+    const inputVerPassConfirm = document.querySelector("#form-pass-confirm")
+
+    //botões
+    const btnVerPass = document.querySelector("#btn-ver-pass")
+    const btnVerPassConfirm = document.querySelector("#btn-ver-pass-confirm")
+    const iconeVerPass = document.querySelector('#btn-ver-pass > i')
+    const iconeVerPassConfirm = document.querySelector('#btn-ver-pass-confirm > i')
+
+    let textPassword = {
+        'text': ( el ) =>{ el.setAttribute ('type', 'password')},
+        'password': ( el ) =>{ el.setAttribute ('type', 'text')},
+    }
+
+    //alternar senha
+    btnVerPass.addEventListener('click', function(){
+        textPassword[inputVerPass.type](inputVerPass)
+    })
+
+    //alternar confirmar senha
+    btnVerPassConfirm.addEventListener('click', function(){
+        textPassword[inputVerPassConfirm.type](inputVerPassConfirm)
+    })
+
 }
